bdec2a25918d5fda567e5bcbb15aa35bfd3a5fe4
KIM-TAE-HWAN <165452706+KIM-TAE-HWAN@users.noreply.github.com>
2024-04-13 14:18:22 +0900

    Add files via upload

---
+++ Day02/src/output/Ex02.java
@@ -0,0 +1,14 @@
+package output;
+
+public class Ex02 {
+	public static void main(String[] arg) {
+		
+		System.out.println("Hello World!");
+		System.out.println("안녕 자바");
+		
+		System.out.println("코드는 위에서 아래로 순차적으로 실행");
+		
+		System.out.println("빈 줄은 실행하지 않는 줄이다");
+		
+	}
+}
---
+++ Day02/src/varialble/Ex07.java
@@ -0,0 +1,11 @@
+package varialble;
+
+public class Ex07 {
+	public static void main(String[] args) {
+		String name = "홍길동";
+		int age = 33;
+		
+		System.out.println("이름 : " + name);
+		System.out.println("나이 : " + age + "세");
+	}
+}
