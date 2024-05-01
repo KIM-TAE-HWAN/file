0b7208507d9a68c636be627636b9dba82596f39a
KIM-TAE-HWAN <165452706+KIM-TAE-HWAN@users.noreply.github.com>
2024-05-01 23:40:40 +0900

    Add files via upload

---
+++ Day71/src/main/java/com/itbank/Controller/AccountController.java
@@ -0,0 +1,87 @@
+package com.itbank.Controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.itbank.model.vo.AccountVO;
+import com.itbank.service.AccountService;
+
+@Controller
+@RequestMapping("/account")
+public class AccountController {
+
+	@Autowired
+	private AccountService as;
+	
+	@GetMapping("/list")
+	public ModelAndView list() {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.addObject("acc", as.getaccounts());
+		
+		return mav;
+	}
+	
+	@GetMapping("/login")
+	public void login() {}
+	
+	@PostMapping("/login")
+	public String login(AccountVO user, HttpSession session) {
+		
+		user = as.login(user);
+		
+		if (user != null) {
+			session.setAttribute("user", user);
+		}
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/myPage")
+	public void myPage() {}
+	
+
+	@PostMapping("/myPage")
+	public String myPage(AccountVO input, HttpSession session) {
+		as.update(input, session);
+		
+		return "redirect:/account/list";
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.removeAttribute("user");
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/signUp")
+	public void signUp() {}
+	
+	@PostMapping("/signUp")
+	public String singUp(AccountVO input) {
+		as.addAccount(input);
+		
+		return "redirect:/account/login";
+	}
+	
+	@GetMapping("/findPw")
+	public void findPw() {}
+	
+	@PostMapping("/findPw")
+	public ModelAndView findPW(AccountVO input) {
+		ModelAndView mav = new ModelAndView();
+		
+		mav.addObject("msg", as.findPw(input));
+		mav.addObject("path", "/account/login");
+		mav.setViewName("mess");
+		
+		return mav;
+	}
+}
---
+++ Day71/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ include file="header.jsp" %>
+
+	
+	<p>1 ~ 10까지의 합 : ${total }</p>
+	<p>로또 번호 : ${lotto }</p>
+</body>
+</html>
